43e659a60d3884ce7cf4e1f4359a13e606fcfc6f
MatasN02 <90383891+MatasN02@users.noreply.github.com>
2024-03-27 13:36:56 +0200

    Update order.java

--- src/main/model/order.java
+++ src/main/model/order.java
@@ -1,20 +1,9 @@
 package lt.eif.viko.mnark.travelagency.model;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 import javax.persistence.*;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElementWrapper;
 import java.util.List;
 
-/**
- * Holds information about Travel Agency Orders.
- *
- * @author Matas Narkunas
- * @since 1.0
- */
 @XmlRootElement
 @XmlType(propOrder = {"id", "orderDate", "travelers", "trips"})
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -37,14 +26,7 @@ public class Order {
     @OneToMany(targetEntity = Trip.class, cascade = CascadeType.ALL)
     private List<Trip> trips;
 
-    public Order(){}
-
-    public Order(int id, String orderDate, List<Traveler> travelers, List<Trip> trips) {
-        this.id = id;
-        this.orderDate = orderDate;
-        this.travelers = travelers;
-        this.trips = trips;
-    }
+    public Order() {}
 
     public Order(String orderDate, List<Traveler> travelers, List<Trip> trips) {
         this.orderDate = orderDate;
@@ -52,62 +34,28 @@ public class Order {
         this.trips = trips;
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setId(int id) { this.id = id; }
 
-    public String getOrderDate() {
-        return orderDate;
-    }
+    public String getOrderDate() { return orderDate; }
 
-    public void setOrderDate(String orderDate) {
-        this.orderDate = orderDate;
-    }
+    public void setOrderDate(String orderDate) { this.orderDate = orderDate; }
 
-    public List<Traveler> getTravelers() {
-        return travelers;
-    }
+    public List<Traveler> getTravelers() { return travelers; }
 
-    public void setTravelers(List<Traveler> travelers) {
-        this.travelers = travelers;
-    }
+    public void setTravelers(List<Traveler> travelers) { this.travelers = travelers; }
 
-    public List<Trip> getTrips() {
-        return trips;
-    }
+    public List<Trip> getTrips() { return trips; }
 
-    public void setTrips(List<Trip> trips) {
-        this.trips = trips;
-    }
+    public void setTrips(List<Trip> trips) { this.trips = trips; }
 
     @Override
     public String toString() {
-        return String.format("Order:\n\t Date = %s\n\t" +
-                        "Travelers: \n\t%s" +
-                        "Trips:\n\t\t%s",
+        return String.format("Order:\n\t Date = %s\n\tTravelers: %s\n\tTrips: %s",
                 this.orderDate,
                 this.travelers,
-                this.trips,
-                constructTravelerString(),
-                constructTripString());
-    }
-
-   private String constructTravelerString(){
-        String resultTraveler = "";
-        for(Traveler traveler : this.travelers){
-            resultTraveler += traveler.toString();
-        }
-        return resultTraveler;
-    }
-    private String constructTripString(){
-        String resultTrip = "";
-        for(Trip trip : this.trips){
-            resultTrip += trip.toString();
-        }
-        return resultTrip;
+                this.trips);
     }
 }
+
