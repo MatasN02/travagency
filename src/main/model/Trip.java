b2f4c8acb022cb09f5f7e027f064194f30381a94
MatasN02 <90383891+MatasN02@users.noreply.github.com>
2024-03-27 13:35:50 +0200

    Update Trip.java

--- src/main/model/Trip.java
+++ src/main/model/Trip.java
@@ -2,12 +2,6 @@ package lt.eif.viko.mnark.travelagency.model;
 
 import javax.persistence.*;
 
-/**
- * Holds information about Trips.
- *
- * @author Matas Narkunas
- * @since 1.0
- */
 @Entity
 @Table(name = "trip")
 public class Trip {
@@ -15,22 +9,10 @@ public class Trip {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private int id;
-    private String organizationName;
-    private String name;
-    private String location;
-    private String tripDuration;
+    private String organizationName, name, location, tripDuration;
     private int cost;
 
-    public Trip(){}
-
-    public Trip(int id, String organizationName, String name, String location, String tripDuration, int cost) {
-        this.id = id;
-        this.organizationName = organizationName;
-        this.name = name;
-        this.location = location;
-        this.tripDuration = tripDuration;
-        this.cost = cost;
-    }
+    public Trip() {}
 
     public Trip(String organizationName, String name, String location, String tripDuration, int cost) {
         this.organizationName = organizationName;
@@ -40,63 +22,34 @@ public class Trip {
         this.cost = cost;
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setId(int id) { this.id = id; }
 
-    public String getOrganizationName() {
-        return organizationName;
-    }
+    public String getOrganizationName() { return organizationName; }
 
-    public void setOrganizationName(String organizationName) {
-        this.organizationName = organizationName;
-    }
+    public void setOrganizationName(String organizationName) { this.organizationName = organizationName; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public String getLocation() {
-        return location;
-    }
+    public String getLocation() { return location; }
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
+    public void setLocation(String location) { this.location = location; }
 
-    public String getTripDuration() {
-        return tripDuration;
-    }
+    public String getTripDuration() { return tripDuration; }
 
-    public void setTripDuration(String tripDuration) {
-        this.tripDuration = tripDuration;
-    }
+    public void setTripDuration(String tripDuration) { this.tripDuration = tripDuration; }
 
-    public int getCost() {
-        return cost;
-    }
+    public int getCost() { return cost; }
 
-    public void setCost(int cost) {
-        this.cost = cost;
-    }
+    public void setCost(int cost) { this.cost = cost; }
 
     @Override
     public String toString() {
-        return String.format("\tTrip:\n\t\t\t\t" + "Organization Name = %s\n\t\t\t\t" + "Name = %s\n\t\t\t\t" +
-                "Location = %s\n\t\t\t\t" + "Trip Duration = %s\n\t\t\t\t" + "Cost = %s\n\t\t\t\t",
-                this.organizationName,
-                this.name,
-                this.location,
-                this.tripDuration,
-                this.cost);
+        return String.format("\tTrip:\n\t\t\t\tOrganization Name = %s\n\t\t\t\tName = %s\n\t\t\t\tLocation = %s\n\t\t\t\tTrip Duration = %s\n\t\t\t\tCost = %s\n\t\t\t\t",
+                organizationName, name, location, tripDuration, cost);
     }
-
 }
+
