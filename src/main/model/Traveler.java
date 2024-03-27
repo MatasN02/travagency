58397055aef34631b6f182790105657a3a5dbf1e
MatasN02 <90383891+MatasN02@users.noreply.github.com>
2024-03-27 13:34:53 +0200

    Update Traveler.java

--- src/main/model/Traveler.java
+++ src/main/model/Traveler.java
@@ -2,12 +2,6 @@ package lt.eif.viko.mnark.travelagency.model;
 
 import javax.persistence.*;
 
-/**
- * Holds information about Travelers.
- *
- * @author Matas Narkunas
- * @since 1.0
- */
 @Entity
 @Table(name="traveler")
 public class Traveler {
@@ -15,52 +9,30 @@ public class Traveler {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="id")
     private int id;
-    private String firstName;
-    private String lastName;
+    private String firstName, lastName;
 
-    public Traveler(){}
-
-    public Traveler(int id, String firstName, String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+    public Traveler() {}
 
     public Traveler(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setId(int id) { this.id = id; }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public String getFirstName() { return firstName; }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    public String getLastName() {
-        return lastName;
-    }
+    public String getLastName() { return lastName; }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
     @Override
-    public String toString(){
-        return String.format("\tName = %s\n\t\t" + "Last Name = %s\n\t\t",
-                this.firstName,
-                this.lastName);
+    public String toString() {
+        return String.format("\tName = %s\n\t\tLast Name = %s\n\t\t", firstName, lastName);
     }
-
-
 }
+
